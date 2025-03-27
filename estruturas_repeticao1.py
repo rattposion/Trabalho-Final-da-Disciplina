42812bd3b21487db03e2755b40ea3fd2ed233b7e
Wesley <142677140+rattposion@users.noreply.github.com>
2025-03-27 07:36:20 -0400

    Create estruturas_repeticao1.py
    estruturas_repeticao1.py → while

---
+++ estruturas_repeticao1.py
@@ -0,0 +1,7 @@
+# Microatividade 3: Estrutura de Repetição while
+
+entrada_idade = ''
+while str(entrada_idade) != '0':
+    entrada_idade = input('Digite um número qualquer ou 0 para sair: ')
+    print(f'Número digitado: {entrada_idade}')
+
