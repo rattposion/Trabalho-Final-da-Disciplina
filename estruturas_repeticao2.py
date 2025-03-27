78d2ddbc778eebe9f889143d7761d81f617ce20f
Wesley <142677140+rattposion@users.noreply.github.com>
2025-03-27 07:37:25 -0400

    Create estruturas_repeticao2.py
    estruturas_repeticao2.py → for

---
+++ estruturas_repeticao2.py
@@ -0,0 +1,9 @@
+# Microatividade 4: Estrutura de Repetição for
+
+texto = 'Olá, laço for.'
+for item in texto:
+    print(f'Caractere: {item}')
+
+for num in range(1, 11):
+    print(f'Número do intervalo: {num}')
+
