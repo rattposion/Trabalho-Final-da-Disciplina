2999ac51b712d1859b5b33b976d649aca819dd6b
Wesley <142677140+rattposion@users.noreply.github.com>
2025-03-27 07:35:30 -0400

    Create estruturas_condicao2.py
    estruturas_condicao2.py → elif

---
+++ estruturas_condicao2.py
@@ -0,0 +1,26 @@
+ #Microatividade 2: Estrutura de Condição elif
+
+tempoExperiencia = 5
+if tempoExperiencia < 2:
+    print('Nível de conhecimento júnior.')
+elif 2 <= tempoExperiencia < 5:
+    print('Nível de conhecimento pleno.')
+else:
+    print('Nível de conhecimento sênior.')
+
+tempoExperiencia = 1
+if tempoExperiencia < 2:
+    print('Nível de conhecimento júnior.')
+elif 2 <= tempoExperiencia < 5:
+    print('Nível de conhecimento pleno.')
+else:
+    print('Nível de conhecimento sênior.')
+
+tempoExperiencia = 3
+if tempoExperiencia < 2:
+    print('Nível de conhecimento júnior.')
+elif 2 <= tempoExperiencia < 5:
+    print('Nível de conhecimento pleno.')
+else:
+    print('Nível de conhecimento sênior.')
+
