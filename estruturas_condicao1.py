b8f1014be2f43db7c0b4ac3728fe81794255d6c6
Wesley <142677140+rattposion@users.noreply.github.com>
2025-03-27 07:33:48 -0400

    Create estruturas_condicao1.py
    estruturas_condicao1.py → if/else

---
+++ estruturas_condicao1.py
@@ -0,0 +1,13 @@
+# Microatividade 1: Estruturas de Condição if e else
+
+temperatura = 29
+if temperatura < 30:
+    print('A temperatura hoje está amena')
+else:
+    print('Hoje está fazendo calor')
+
+temperatura = 31
+if temperatura < 30:
+    print('A temperatura hoje está amena')
+else:
+    print('Hoje está fazendo calor')
